ced16c5aa2d311fb426f7a19d1e6c20b3ac2e2ec
keshavlabs <kkarwa04@gmail.com>
2026-05-26 19:03:40 +0530

    Fix render CORS

--- apps/api/src/plugins/cors.js
+++ apps/api/src/plugins/cors.js
@@ -1,13 +1,8 @@
 import cors from "@fastify/cors";
 
 export default async function corsPlugin(fastify) {
-  fastify.register(cors, {
-    origin: [
-      "http://localhost:3000",
-      "http://127.0.0.1:3000",
-      "https://linklytics-web.vercel.app",
-    ],
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+  await fastify.register(cors, {
+    origin: true,
+    credentials: false,
   });
 }
