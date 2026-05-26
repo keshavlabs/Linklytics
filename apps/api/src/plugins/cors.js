bfcaaa3e2b52c20882f5106b93d66f2c8911f354
keshavlabs <kkarwa04@gmail.com>
2026-05-26 18:09:51 +0530

    Changing URLs

--- apps/api/src/plugins/cors.js
+++ apps/api/src/plugins/cors.js
@@ -1,9 +1,12 @@
 import cors from "@fastify/cors";
-import { env } from "../config/env.js";
 
 export default async function corsPlugin(fastify) {
   fastify.register(cors, {
-    origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
+    origin: [
+      "http://localhost:3000",
+      "http://127.0.0.1:3000",
+      "https://linklytics-web.vercel.app",
+    ],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
